b61d3077fe8238e0cf5ab294d0c91ebf12c8a529
diominor <backgom2357@gmail.com>
2021-12-02 01:53:47 +0900

    beta:0.1

--- env.py
+++ env.py
@@ -1,5 +1,7 @@
 import numpy as np
+import cv2
 from data.main import *
+from copy import deepcopy
 
 class env:
     def __init__(self,
@@ -14,7 +16,6 @@ class env:
         self.image_list = image_list
         if self.image_list == None:
             self.image_list = self.generate_images(image_size, prob_cutout, num_images)
-        self.image_size = self.image_list[0].shape[0]
                 
         # info
         '''
@@ -24,8 +25,8 @@ class env:
         {0 : 동작 안함, 1 : 좌측 상단, 2 : 좌측, 3 : 좌측 하단,
         4 : 우측 상단, 5 : 우측, 6 : 우측 하단}
         '''
-        self.state_dim = image_list[0].shape
-        self.n_action = 7
+        self.state_dim = self.image_list[0].shape
+        self.action_space = 7
         self.all_directions = [(0,-1),  #↑
                                (1,-1),  #↗
                                (1,0),   #→
@@ -36,40 +37,48 @@ class env:
                                (-1,-1)] #↖
         
         # initialize
-        self.current_image = self.image_list[np.random.randint(len(self.img_list))]
+        self.current_image = deepcopy(self.image_list[np.random.randint(len(self.image_list))])
+        self.image_size = self.current_image.shape[0]
 
+        candidate_positions = [(0,0), (0,self.image_size-1),(self.image_size-1,0),(self.image_size-1, self.image_size-1)]
+        
         self.start_position = start_position
         if self.start_position == None:
-            self.current_position = np.random.choice([(0,0), 
-                                                    (0,self.image_size),
-                                                    (self.image_size,0),
-                                                    (self.image_size, self.image_size)], 1)
+            self.start_position = candidate_positions[np.random.choice(len(candidate_positions), 1)[0]]
+        self.current_position = list(self.start_position)
 
         self.start_direction = start_direction
         if self.start_direction == None:
-            if self.current_position == (0,0): 
-                self.current_direction = np.random.choice([self.all_directions[2], self.all_directions[4]],1)
-            elif self.current_position == (0,self.image_size): 
-                self.current_direction = np.random.choice([self.all_directions[0], self.all_directions[2]],1)
-            elif self.current_position == (self.image_size,0): 
-                self.current_direction = np.random.choice([self.all_directions[4], self.all_directions[6]],1)
-            elif self.current_position == (self.image_size, self.image_size): 
-                self.current_direction = np.random.choice([self.all_directions[0], self.all_directions[6]],1)
+            if self.current_position == list(candidate_positions[0]): 
+                direction_index = np.random.choice([2, 4],1)
+            elif self.current_position == list(candidate_positions[1]): 
+                direction_index = np.random.choice([0, 2],1)
+            elif self.current_position == list(candidate_positions[2]): 
+                direction_index = np.random.choice([4, 6],1)
+            elif self.current_position == list(candidate_positions[3]): 
+                direction_index = np.random.choice([0, 6],1)
+            self.start_direction = self.all_directions[direction_index[0]]
 
-        self.start_direction = list(self.start_direction)
+        self.current_direction = self.start_direction
         self.time = 0
+        
+        self.fill_up_done = -(self.image_size**2) + 2*np.sum(self.current_image)
+        self.time_end_done = (self.image_size**2)*2
     
-    def generate_imgs(self, img_size, prob_cutout, num_images):
+    def generate_images(self, img_size, prob_cutout, num_images):
         image_list = []
         for i in range(num_images):
             image_list.append(square_gen(img_size, prob_cutout))
         return image_list
     
     def reset(self):
-        image_index = np.random.randint(len(self.img_list))
+        image_index = np.random.randint(len(self.image_list))
         self.current_image = self.image_list[image_index]
-        self.current_position = self.start_position
+        self.image_size = self.current_image.shape[0]
+        self.current_position = list(self.start_position)
+        self.current_image[self.current_position[1]][self.current_position[0]] = -1
         self.time = 0
+        return self.current_image
         
     def change_direction(self, action):
         '''
@@ -93,25 +102,60 @@ class env:
             degree = 135
         
         transformation_matrix = self.generate_transformation_matrix(degree)
-        self.current_direction = tuple(np.array(self.current_direction).dot(transformation_matrix))
+        self.current_direction = tuple(np.array(self.current_direction).dot(transformation_matrix).round(0).astype(np.int8))
     
     def generate_transformation_matrix(self, degree):
+        degree *= (np.pi / 180.)
         if degree == 90 or degree == -90:
             return np.array([[np.cos(degree), np.sin(degree)],[-np.sin(degree), np.cos(degree)]])
         else:
-            return np.array([[np.cos(degree), np.sin(degree)],[-np.sin(degree), np.cos(degree)]])/np.cos(degree)
+            return np.array([[np.cos(degree), np.sin(degree)],[-np.sin(degree), np.cos(degree)]])
             
     def step(self, action):
         # change direction
         self.change_direction(action)
         
         # move
-        self.old_position = self.current_position
-        self.current_position[0], self.current_position[1] += self.current_direction[0], self.current_direction[1]
-        # not move at the edges
-        if self.current_position.any() < 0 or self.current_position.any() > self.image_size:
-            self.current_direction = self.old_position
+        self.old_position = deepcopy(self.current_position)
+        self.current_position[0] += self.current_direction[0]
+        self.current_position[1] += self.current_direction[1]
+        if (0<=self.current_position[0]<self.image_size) and (0<=self.current_position[1]<self.image_size): # not move at the edges
+            x, y = self.current_position
+            if self.current_image[y][x] in [-1, 1]:
+                self.time += 0.5
+            elif self.current_image[y][x] == 0:
+                self.current_image[y][x] = -1
+                self.time += 1
+        else:
+            self.current_position = self.old_position
+            self.time += 1
         
-    
+        done = False
+        reward = 0
+        if self.time >= self.time_end_done or np.sum(self.current_image) == self.fill_up_done:
+            done = True
+            reward = -self.time
+            
+        return self.current_image, reward, done
+                
     def render(self):
-        pass
+        image = (deepcopy(self.current_image) + 1)/2
+        image[self.current_position[1]][self.current_position[0]] = 0.3
+        render_img = cv2.resize(image, dsize=(400, 400), interpolation=cv2.INTER_AREA)
+        cv2.imshow('Molding...', render_img)
+        if cv2.waitKey(25)==ord('q'):
+            cv2.destroyAllWindows()
+
+import time
+if __name__=="__main__":
+    env = env()
+    obs = env.reset()
+    max_step = 100000
+    step = 0
+    while step < max_step:
+        env.render()
+        action = np.random.randint(7)
+        _,_,done = env.step(action)
+        if done:
+            break
+    
